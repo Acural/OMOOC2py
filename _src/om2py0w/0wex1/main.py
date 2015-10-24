3a4bd12487a50daf1ce2108ab530559fea738372
Acural <acural@163.com>
2015-10-24 20:40:15 +0800

    beat2   creat time model

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,15 +1,27 @@
 from sys import argv
 script , filename = argv
+import time   #调用时间函数
+
+now = time.strftime ("%Y-%m-%d %H:%M:%S")  #  显示载入程序时的时间
+print "now time is : %r" % now
+print "="*20
 
 print "let's start writing"
 line1 = raw_input ()
+now1 = time.strftime ("%Y-%m-%d %H:%M:%S")  #读取写作时的时间
 
 target = open(filename,'a')
+target.write("creatime")
+target.write(":")
+target.write(now1)
+target.write("\n")
 target.write(line1)
 target.write("\n")
-target.write("\n")
+target.write("\n")                    #两个空行起美观作用
 target.close()
 
-print "Here's your daily"
+print "Here's your daily:"
+print "="*20
 daily = open(filename)
 print daily.read()
+print "="*20
