8c0e4ee961b83fd0f7b48f838d789876e4afcb2c
Acural <acural@163.com>
2015-10-24 17:35:31 +0800

    bug solved

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -4,6 +4,7 @@ script , filename = argv
 print "let's start writing"
 line1 = raw_input ()
 
-target = open(filename,'w')
+target = open(filename,'a')
 target.write(line1)
 target.write("\n")
+target.close()
